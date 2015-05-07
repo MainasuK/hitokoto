4f1e313fdad1f840971904022eae918db9f1745c
MainasuK <cirno.mainasuk@gmail.com>
2015-05-07 23:19:06 +0800

    init commit

---
+++ hitokoto/hitokoto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hitokoto
+//
+//  Created by Cirno MainasuK on 2015-5-7.
+//  Copyright (c) 2015年 Cirno MainasuK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
