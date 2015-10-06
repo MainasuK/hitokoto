ebd8eb2c3abd5521363f14f4473b5d879b635fb0
MainasuK <cirno.mainasuk@gmail.com>
2015-10-06 18:23:31 +0800

    - Add share sheet menu

--- hitokoto/hitokoto/HitokotoData/HitokotoService.swift
+++ hitokoto/hitokoto/HitokotoData/HitokotoService.swift
@@ -9,48 +9,45 @@
 import Foundation
 
 // Create an error type which you'll use later
-enum HitokotoServiceError: ErrorType {
+public enum HitokotoServiceError: ErrorType {
 	case InvalidRetrievedDictionary
 	case InvalidURLFormat(format: String)
+    case NSURLError(format: String)
 }
 
 final class HitokotoService { // If you don't need to create another sub class inherited from this one, add a final prefix
     
     typealias HitokotoDataCompletionBlock = (data: HitokotoData?, error: ErrorType?) -> ()
     
+    private let kTimeout = NSTimeInterval(5.0)
     private let session: NSURLSession // Use as strict scope as possible. You don't need to let everybody see the proberties.
 	
 	static let sharedInstance = HitokotoService() // Since Swift 1.2 you don't need the ugly extra struct contanind in the class var anymore.
-//    class var sharedInstance: HitokotoService {
-//        struct Singleton {
-//            static let instance = HitokotoService()
-//        }
-//        return Singleton.instance
-//    }
 	
     private init() { // Basically singleton design pattern is not so recommended but if you'd really like to use it, remember to hide the initializer.
         session = NSURLSession(configuration: .defaultSessionConfiguration())
+
     }
     
     func fetchHitokotoData(format: String, completion: HitokotoDataCompletionBlock) {
-        
+
 		guard let baseUrl = NSURL(string: "http://api.hitokoto.us/rand?\(format)") else { // Throw an error rather than using forced unwrapping
 			completion(data: nil, error: HitokotoServiceError.InvalidURLFormat(format: format))
 			return
 		}
-		
-        let request = NSURLRequest(URL: baseUrl)
-        print(baseUrl) // You don't need to do the forced formatting since the result is definitely the same.
-		
+        
+        print("Fetching data from: \(baseUrl)…")
+        let request = NSURLRequest(URL: baseUrl, cachePolicy: NSURLRequestCachePolicy.ReloadIgnoringLocalAndRemoteCacheData, timeoutInterval: kTimeout)
         let task = session.dataTaskWithRequest(request) { data, response, error in
             
-			guard let data = data where error == nil  else { // If there's an error use guard rather than if which is easier to understand the logic. Also you may unwrap the optional value here so you don't need to forced unwrapping it later.
-				completion(data: nil, error: error)
+			guard let data = data where error == nil  else {
+                print(error!.localizedDescription)
+				completion(data: nil, error: HitokotoServiceError.NSURLError(format: error!.localizedDescription))
 				return
 			}
 			
 			do {
-				guard let hitokotoDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? [String: NSObject] else { // Throw an error rather than using forced unwrapping, and specify the swift dictionary instance rather than using Objective-C NSDictionary object
+				guard let hitokotoDictionary = try NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.MutableContainers) as? [String: NSObject] else {
 					throw HitokotoServiceError.InvalidRetrievedDictionary
 				}
 				let data = try HitokotoData(hitokotoDictionary: hitokotoDictionary)
